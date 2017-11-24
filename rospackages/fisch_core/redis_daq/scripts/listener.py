fb2040343a32b674dad18438da265973c3a4a86e
wqkhan <wqkhan@uwaterloo.ca>
2017-11-24 12:24:21 -0500

    Updae README

--- rospackages/fisch_core/redis_daq/scripts/listener.py
+++ rospackages/fisch_core/redis_daq/scripts/listener.py
@@ -47,7 +47,7 @@ def navsat_fix_callback(data):
     # Current time
     now_ = rospy.get_rostime()
 
-    msg = [now_, data.status, data.latitude, data.longitude, data.altitude]
+    msg = [now_, data.status.rstrip(), data.latitude, data.longitude, data.altitude]
     str_msg = ','.join(map(str, msg)) 
 
     con.publish("_navsat_fix", str_msg)
