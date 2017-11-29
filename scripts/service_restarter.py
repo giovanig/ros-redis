e83595b8a9517dadc8056d059cfdfe7d7220b088
wqkhan <wqkhan@uwaterloo.ca>
2017-11-29 16:12:02 -0500

    Update rosackage

--- scripts/service_restarter.py
+++ scripts/service_restarter.py
@@ -2,8 +2,22 @@
 
 # https://gist.github.com/ewenchou/be496b2b73be801fd85267ef5471458c
 
+# cd /usr/bin
+# sudo ln -sfn /home/dev/fisch/ros-redis/scripts/service_restarter.py
+
+# cd /lib/systemd/system
+# sudo vim fisch_nodeCheck.service
+
 import sys, os, time
 import subprocess
 
+import rospy, rosnode
+
 if __name__ == '__main__':
 
+    process = subprocess.Popen("ssh root@192.168.200.101 rosnode list  | grep something",
+                shell=True,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            )
+    (stdoutdata, stderrdata) = proc.communicate()
+    print(stdoutdata)
