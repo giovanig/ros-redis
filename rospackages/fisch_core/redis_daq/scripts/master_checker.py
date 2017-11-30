23c33ed34e50202b7ca763ef28f0b78dc56a85ad
waleedqk <wqkhan@uwaterloo.ca>
2017-11-30 15:48:41 -0500

    Update rosackage

--- rospackages/fisch_core/redis_daq/scripts/master_checker.py
+++ rospackages/fisch_core/redis_daq/scripts/master_checker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys, os, time
 import subprocess
 import redis
@@ -16,12 +18,18 @@ if __name__ == '__main__':
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 env=my_env,
             )
-            (_, stderrdata) = proc.communicate()
+            (stdoutdata, stderrdata) = proc.communicate()
             # print(stderrdata)
             
             if len(stderrdata.strip()) != 0:
                 print("ERROR: Unable to communicate with master! - Exiting")
                 sys.exit(1)
             else:
-                print("No errors")
-                time.sleep(2)
+                rosnode_list = stdoutdata.split('\n')
+                if "/rosout" in rosnode_list:
+                    if "/redis_listener" in rosnode_list:
+                        # print("No errors")
+                        time.sleep(2)
+                    else:
+                        print("redis_listener node not found in rosnode list")
+                        sys.exit(1)
