671e30fe77a8d0ac484f51e40da1684132e2d539
waleedqk <wqkhan@uwaterloo.ca>
2017-12-04 17:15:13 -0500

    Update node to publish rosmsg in json format

--- rospackages/fisch_core/redis_daq/scripts/rosmsg2redis_json.py
+++ rospackages/fisch_core/redis_daq/scripts/rosmsg2redis_json.py
@@ -51,7 +51,7 @@ def rosmag_redis_json(data,topic):
   
     con.publish(topic, json_str)
 
-    if topic.strip() == "_vehicle_brak_report":
+    if topic.strip() == "_vehicle_brake_report":
         # var to check if the car is in autonomy mode
         autonomy_status =  '{"_autonomy_state" : {"status" : %s} }' % (data.enabled)
         # print(autonomy_status)
@@ -67,7 +67,7 @@ def topicListener():
     rospy.Subscriber("navsat/fix", NavSatFix, callback = rosmag_redis_json, callback_args =  "_navsat_fix")
     rospy.Subscriber("vehicle/throttle_report", dbw_mkz_msgs.ThrottleReport, callback = rosmag_redis_json, callback_args =  "_vehicle_throttle_report")
     rospy.Subscriber("vehicle/steering_report", dbw_mkz_msgs.SteeringReport, callback = rosmag_redis_json, callback_args =  "_vehicle_steering_report")
-    rospy.Subscriber("vehicle/brake_report", dbw_mkz_msgs.BrakeReport, callback = rosmag_redis_json, callback_args =  "_vehicle_brak_report")
+    rospy.Subscriber("vehicle/brake_report", dbw_mkz_msgs.BrakeReport, callback = rosmag_redis_json, callback_args =  "_vehicle_brake_report")
     rospy.Subscriber("vehicle/gear_report", dbw_mkz_msgs.GearReport, callback = rosmag_redis_json, callback_args =  "_vehicle_gear_report")
     rospy.Subscriber("vehicle_state", anm_msgs.VehicleState, callback = rosmag_redis_json, callback_args =  "_vehicle_state_report")
     rospy.Subscriber("vehicle/p1hc_enable_flag", Bool, callback = rosmag_redis_json, callback_args =  "_vehicle_p1hc_enable_flag")
