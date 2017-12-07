ad869c3fe6938ce60ebee67aa23584b8b691e358
wqkhan <wqkhan@uwaterloo.ca>
2017-12-07 15:33:21 -0500

    Add p1hc udp capability in new package

--- setup/deploy/opt/ros/fisch/lib/redis_daq/master_checker.py
+++ setup/deploy/opt/ros/fisch/lib/redis_daq/master_checker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys, os, time
-import subprocess
+import subprocess, logging
 import redis
 import rospy
 import rosnode
@@ -9,6 +9,23 @@ import rosnode
 my_env = os.environ.copy()
 my_env["ROS_MASTER_URI"] = "http://192.168.200.101:11311" 
 my_env["ROS_IP"] = "192.168.200.100"
+
+# path to directory of python file
+dirname, filename = os.path.split(os.path.abspath(__file__))
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# create a file handler
+handler = logging.FileHandler('periodic.log')
+handler.setLevel(logging.DEBUG)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
 
 if __name__ == '__main__':
     # give enough time for the node to be up and running before checking
@@ -24,14 +41,14 @@ if __name__ == '__main__':
             # print(stderrdata)
             
             if len(stderrdata.strip()) != 0:
-                print("ERROR: Unable to communicate with master! - Exiting")
+                logger.warning("ERROR: Unable to communicate with master! - Exiting")
                 sys.exit(1)
             else:
                 rosnode_list = stdoutdata.split('\n')
                 if "/rosout" in rosnode_list:
                     if "/rosmsg2redis_json" in rosnode_list:
-                        # print("No errors")
+                        logger.info("ROS node still active")
                         time.sleep(2)
                     else:
-                        print("rosmsg2redis_json node not found in rosnode list")
+                        logger.warning("rosmsg2redis_json node not found in rosnode list")
                         sys.exit(1)
